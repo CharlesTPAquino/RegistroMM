0b4955ce21ae4de649a8344e85437c4245fafe8f
Charles Aquino <mycode.indev@gmail.com>
2025-04-06 23:41:38 -0300

    Configuração do GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -12,5 +13,6 @@ export default defineConfig({
     port: 3010,
     strictPort: true,
     host: true
-  }
-});
+  },
+  base: '/RegistroMM/',
+})
