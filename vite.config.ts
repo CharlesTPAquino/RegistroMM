fa66b1a7956bfa238dcdfa6fc88a29a500a9fc20
Charles Aquino <mycode.indev@gmail.com>
2025-04-07 04:37:38 -0300

    Otimizando configuração do Vite para build

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   base: '/RegistroMM/',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false,
+    emptyOutDir: true,
     rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+      },
       output: {
-        manualChunks: undefined,
-        entryFileNames: '[name].[hash].js',
-        chunkFileNames: '[name].[hash].js',
-        assetFileNames: '[name].[hash].[ext]'
+        entryFileNames: `assets/[name].[hash].js`,
+        chunkFileNames: `assets/[name].[hash].js`,
+        assetFileNames: `assets/[name].[hash].[ext]`
       }
     }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   }
 })
