c93a5e29ac21c60f4658950e1fa4f1d867b2cf57
Charles Aquino <mycode.indev@gmail.com>
2025-04-07 04:43:44 -0300

    Simplificando configuração e corrigindo workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import * as path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   base: '/RegistroMM/',
   build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html'),
-      },
-      output: {
-        entryFileNames: `assets/[name].[hash].js`,
-        chunkFileNames: `assets/[name].[hash].js`,
-        assetFileNames: `assets/[name].[hash].[ext]`
-      }
-    }
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
+    outDir: 'dist'
   }
 })
